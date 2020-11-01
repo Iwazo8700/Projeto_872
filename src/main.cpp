adc6154f4040d27bc47642346e9a3b3adb2aa0d1
João Gabriel Segato Kruse <j218615@dac.unicamp.br>
2020-11-01 13:42:55 -0300

    Formato do mapa arrumado

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,14 @@
 int main(){
 	std::vector<std::vector<bool>> vec(4, std::vector<bool>(4, true));
 	std::shared_ptr<Formato> format (new Formato("../assets/Formatos.dat"));
-	std::shared_ptr<SDL_Model> sdl (new SDL_Model(800,800));
+	std::shared_ptr<SDL_Model> sdl (new SDL_Model(200,400));
 	std::shared_ptr<View> view (new View(sdl));
 	std::shared_ptr<Sprite> sprite (new Sprite("../assets/Bloco.png",sdl));
 	std::shared_ptr<Sprite> sprite2 (new Sprite("../assets/Fundo.png",sdl));
-	std::shared_ptr<Map> map (new Map(10,20,20,20));
-	std::shared_ptr<Image> img (new Image(0,0,800,800,sprite2));
+	std::shared_ptr<Map> map (new Map(20,20,20,20));
+	std::shared_ptr<Image> img (new Image(0,0,200,400,sprite2));
 	std::shared_ptr<Bloco> block (new Bloco(0,0,vec,sprite,20,20));
-	std::shared_ptr<BlockPosition> blk_pos(new BlockPosition(20, 20));
+	std::shared_ptr<BlockPosition> blk_pos(new BlockPosition(0, 0));
 	std::vector<std::shared_ptr<Sprite>> vecin;
 	vecin.push_back(sprite);
 	vecin.push_back(sprite2);
