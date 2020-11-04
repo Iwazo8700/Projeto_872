97fa877741f34f37f2dc1ec53339a800fffd40c3
João Gabriel Segato Kruse <j218615@dac.unicamp.br>
2020-11-04 14:14:56 -0300

    Nomes das funções no CMakeFile atualizados

---
+++ build/CMakeFiles/SDL_Funcs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL_Funcs.dir/src/Model/SDL_Model.cpp.o"
+  "libSDL_Funcs.a"
+  "libSDL_Funcs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL_Funcs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Sirtet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sirtet.dir/src/main.cpp.o"
+  "Sirtet"
+  "Sirtet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sirtet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
